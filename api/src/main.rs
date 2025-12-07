c3ed09ad32910680d0cdac080991b871212ee700
Zexa <zexa@cock.li>
2025-12-07 21:31:10 +0200

    chore: simplify

--- api/src/main.rs
+++ api/src/main.rs
@@ -2,43 +2,39 @@ use foreign::ForeignType;
 use jsonwrap::{Foreign, ForeignOpt};
 use poem::{Route, Server, listener::TcpListener};
 use poem_openapi::{OpenApi, OpenApiService, payload::Json};
-use serde::{Deserialize, Serialize};
-
-#[derive(Debug, Serialize, Deserialize)]
-struct ForeignTypeWrapper(ForeignType);
 
 struct Api;
 
 #[OpenApi]
 impl Api {
     #[oai(path = "/hello", method = "get")]
-    async fn hello(&self) -> Json<Foreign<ForeignTypeWrapper>> {
-        Json(Foreign(ForeignTypeWrapper(ForeignType {
+    async fn hello(&self) -> Json<Foreign<ForeignType>> {
+        Json(Foreign(ForeignType {
             text: "hello".to_string(),
-        })))
+        }))
     }
 
     #[oai(path = "/optional", method = "get")]
-    async fn optional(&self) -> Json<Option<Foreign<ForeignTypeWrapper>>> {
-        Json(Some(Foreign(ForeignTypeWrapper(ForeignType {
+    async fn optional(&self) -> Json<Option<Foreign<ForeignType>>> {
+        Json(Some(Foreign(ForeignType {
             text: "optional value".to_string(),
-        }))))
+        })))
     }
 
     #[oai(path = "/optional-none", method = "get")]
-    async fn optional_none(&self) -> Json<Option<Foreign<ForeignTypeWrapper>>> {
+    async fn optional_none(&self) -> Json<Option<Foreign<ForeignType>>> {
         Json(None)
     }
 
     #[oai(path = "/foreign-opt", method = "get")]
-    async fn foreign_opt(&self) -> Json<ForeignOpt<ForeignTypeWrapper>> {
-        Json(ForeignOpt(Some(ForeignTypeWrapper(ForeignType {
+    async fn foreign_opt(&self) -> Json<ForeignOpt<ForeignType>> {
+        Json(ForeignOpt(Some(ForeignType {
             text: "using ForeignOpt".to_string(),
-        }))))
+        })))
     }
 
     #[oai(path = "/foreign-opt-none", method = "get")]
-    async fn foreign_opt_none(&self) -> Json<ForeignOpt<ForeignTypeWrapper>> {
+    async fn foreign_opt_none(&self) -> Json<ForeignOpt<ForeignType>> {
         Json(ForeignOpt(None))
     }
 }
@@ -51,7 +47,12 @@ async fn main() {
     let spec = api.spec_endpoint();
 
     Server::new(TcpListener::bind("127.0.0.1:3000"))
-        .run(Route::new().nest("/", api).nest("/docs", ui).nest("/spec.json", spec))
+        .run(
+            Route::new()
+                .nest("/", api)
+                .nest("/docs", ui)
+                .nest("/spec.json", spec),
+        )
         .await
         .unwrap();
 }
